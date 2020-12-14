b89a7990dac79b7a17a9046ac6a0a3c0adf9ab12
dyushakolesov <dyushakolesov@mail.ru>
2020-12-14 17:28:20 +0300

    more vector operations

--- image_handler.py
+++ image_handler.py
@@ -2,39 +2,35 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 
-img1 = mpl.image.imread('img/image1.jpg')#left image
-img2 = mpl.image.imread('img/image2.jpg')#right image
+img1 = mpl.image.imread('img/img1.jpg')#left image
+img2 = mpl.image.imread('img/img2.jpg')#right image
 fig, axs = plt.subplots(1,3)
 
 idx_elem = 0
 idx = 0
 
-
-A = img1[:,:,:]
-B = img2[:,:,:]
+#for example we read only blue channel
+A = img1[:,:,0]
+B = img2[:,:,0]
 
 idx_left = 0
 idx_right = 0
 min_times = 0
-minsum =  np.sum(abs(A[:,-1,:] - B[:,0,:]))
+minsum =  np.sum(abs(A[:,-1] - B[:,0]))
 
 #slice defect images that is why we use estimation: sum of the differens in pixel values
-for i in reversed(np.arange(img1.shape[1])):
-    for j in np.arange(img2.shape[1]):
-
-
-        x = A[:,i,:]
-        y = B[:,j,:]
-        k = np.sum(abs(x-y))
-        if (k < minsum):
+for j in range(B.shape[1]):
+        b = B[:, j]
+        k = np.sum(np.abs(A - b.reshape(-1,1)), axis=0)
+        i = np.argmin(k)
+        if (np.min(k) < minsum):
             idx_left = i
             idx_right = j
-            minsum = k
+            minsum = np.min(k)
             min_times+=1
-        elif (min_times > 6):#unnececary but speeds up splice significantly
-            break
-res_img = np.concatenate((A[:,:idx_left,:],B[:,idx_right:,:]), axis=1) 
-#res_img = B[:,:,:]
+
+res_img = np.concatenate((A[:,:idx_left],B[:,idx_right:]), axis=1)
+
 axs[0].imshow(img1)
 axs[1].imshow(img2)
 axs[2].imshow(res_img)
